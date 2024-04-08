d6660d1e96100ae84dad9a6313597eb8a504512e
sarthakportal2 <77792446+sarthakportal2@users.noreply.github.com>
2024-04-08 12:59:52 +0530

    StoneGame.cs

---
+++ StoneGame.cs
@@ -0,0 +1,12 @@
+public class Solution {
+    //T(C(N))==O(1) and S(C(N)==O(1)) as it requires non additional space in constant time
+  //Using Mathematical approach(Optimal Solution)
+    public bool StoneGame(int[] piles) {return true;}}//printing true 
+  //Using Dynamic Progamming(DP) approach
+        // int n;
+        // //int sum1=0;
+        // for(int i=0;i<piles.Length;i++){
+        //     if(piles[i]==0)return 0;
+        //     else if(n%2==0 && n>0)return false;
+        //     else if(piles[i]>0)piles[i]=piles[i-1];return true;
+        // }return false;}}
