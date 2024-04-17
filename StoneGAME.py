3781e4ed9b0909757a8b1fee7127d88453cb3d33
sarthakportal2 <77792446+sarthakportal2@users.noreply.github.com>
2024-04-17 13:03:10 +0530

    StoneGAME.py

--- StoneGAME.py
+++ StoneGAME.py
@@ -1,5 +1,6 @@
 class Solution:
     def stoneGame(self, piles: List[int]) -> bool:
+        #Iterative_Approach
         #T(C(N)=O(N)) and S(C(N))=O(1) as it requires non memory space allocation iteratively 
         n=int()#Stone(n) declare
         for i in range(len(piles)):#iterating Through Pile's Stack's Length
@@ -7,4 +8,5 @@ class Solution:
             elif n%2==0 and n>0:return False#Printing False to the Pile's Even Number of Stones
             elif piles[i]>0:piles[i]=piles[i-1];return True#printing True to Winner with the Pile's Start till its End  Odd Number Stone 
         return False#printing False to the Unwinner 
-    #def stoneGame(self, piles: List[int]) -> bool:return True
+        #optimal_Solution 
+        #def stoneGame(self, piles: List[int]) -> bool:return True
